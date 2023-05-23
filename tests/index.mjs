d274ab4ae5900718a9b5477b3024170f03cc508f
Govind Rai <raigovind93@gmail.com>
2023-05-23 09:03:35 -0700

    update dockerfile and test to be clear

--- tests/index.mjs
+++ tests/index.mjs
@@ -9,15 +9,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 async function mini_test() {
-    const imageBuffer = fs.readFileSync(path.join(__dirname, "/fixtures/sample1_orig_.heic"))
+    const filename = "img.heic"
+    const imageBuffer = fs.readFileSync(path.join(__dirname, "fixtures", filename))
 
-    // Resize the image using Sharp and convert to WebP
     const resizedImageBuffer = await sharp(imageBuffer)
         .resize({ width: 960, height: 960, fit: 'inside' })
         .toBuffer();
 
-    fs.writeFileSync(path.join(__dirname, "sample1_web_.webp"), resizedImageBuffer)
-    console.log("It might have worked!! check tests directory for sample1_web_.webp")
+    fs.writeFileSync(path.join(__dirname, filename), resizedImageBuffer)
 }
 
 mini_test()
